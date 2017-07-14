7866f7f9879047c1c5c7e58b3333c885469cdf8f
YuChenHeMTL <yuchen7568@gmail.com>
2017-07-14 21:33:43 +0000

    fixed port

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 
 // Basic express setup:
 const methodOverride = require("method-override")
-const PORT          = 8080;
+const PORT          = process.env.PORT || 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
